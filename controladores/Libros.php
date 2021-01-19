dfa64ed37848e1f9a72ac03f1ad842cb8f50f266
JuanC <maijujoshproyects@gmail.com>
2021-01-18 23:05:10 -0600

    Ca1801_23:04

--- controladores/Libros.php
+++ controladores/Libros.php
@@ -4,5 +4,6 @@ require_once("modelos/Libros.php");
 //CREAR LA IINSTANCIA DE LIBROS
 $libro = new Libros();
 $datos = $libro->getLibros();
-var_dump($datos);
+//Llamamos a la Vista
+require_once("vistas/Libros.php");
 ?>
